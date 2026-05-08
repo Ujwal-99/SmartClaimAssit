fb931da0c86ae22d3b414a383bead1ad380f32c3
Ujwal-99 <kujwal324@gmail.com>
2026-05-08 18:03:52 +0530

    Update Register.tsx

--- your-plan-advisor-main/your-plan-advisor-main/src/pages/Register.tsx
+++ your-plan-advisor-main/your-plan-advisor-main/src/pages/Register.tsx
@@ -35,7 +35,7 @@ export default function Register() {
 
     try {
 
-      const res = await fetch("http://localhost:5000/api/register", {
+      const res = await fetch("https://smartclaimassit.onrender.com/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
