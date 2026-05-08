cdf15281f367f6ec9e7832951ed58891cae9a797
Ujwal-99 <kujwal324@gmail.com>
2026-05-08 18:03:06 +0530

    Update Login.tsx

--- your-plan-advisor-main/your-plan-advisor-main/src/pages/Login.tsx
+++ your-plan-advisor-main/your-plan-advisor-main/src/pages/Login.tsx
@@ -29,7 +29,7 @@ export default function Login() {
 
     try {
 
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://smartclaimassit.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
